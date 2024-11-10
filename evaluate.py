9540041bf793bbd08a3a19bbf9d0e84c3530f021
SanjanaRitika <sanjanakarim24@gmail.com>
2024-11-10 13:20:21 +0600

    more patterns added

--- evaluate.py
+++ evaluate.py
@@ -1,59 +1,94 @@
 from boardstate import *
 
-WHITE_6PATTERNS = [['empty', 'white', 'white', 'white', 'white','empty'],
-                   ['empty', 'white', 'white', 'white', 'empty','empty'],
-                   ['empty', 'empty', 'white', 'white', 'white','empty'],
-                   ['empty', 'white', 'white', 'empty', 'white','empty'],
-                   ['empty', 'white', 'empty', 'white', 'white','empty'],
-                   ['empty', 'empty', 'white', 'white', 'empty','empty'],
-                   ['empty', 'empty', 'white', 'empty', 'white','empty'],
-                   ['empty', 'white', 'empty', 'white', 'empty','empty'],
-                   ['empty', 'empty', 'white', 'empty', 'empty','empty'],
-                   ['empty', 'empty', 'empty', 'white', 'empty','empty']]
+WHITE_6PATTERNS = [
+    ['empty', 'white', 'white', 'white', 'white', 'empty'],
+    ['empty', 'white', 'white', 'white', 'empty', 'empty'],
+    ['empty', 'empty', 'white', 'white', 'white', 'empty'],
+    ['empty', 'white', 'white', 'empty', 'white', 'empty'],
+    ['empty', 'white', 'empty', 'white', 'white', 'empty'],
+    ['empty', 'empty', 'white', 'white', 'empty', 'empty'],
+    ['empty', 'empty', 'white', 'empty', 'white', 'empty'],
+    ['empty', 'white', 'empty', 'white', 'empty', 'empty'],
+    ['empty', 'empty', 'white', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'white', 'empty', 'empty'],
+    ['white', 'white', 'empty', 'white', 'white', 'empty'],
+    ['empty', 'white', 'white', 'white', 'empty', 'white'],
+    ['white', 'empty', 'white', 'empty', 'white', 'empty']
+]
 
-WHITE_6SCORES = [50000,5000,5000,500,500,100,100,100,10,10]
-# WHITE_6SCORES = [8640,720,720,720,720,120,120,120,20,20] #based on Dong (2015)
+WHITE_6SCORES = [50000, 5000, 5000, 500, 500, 100, 100, 100, 10, 10, 2500, 1500, 1000]
 
+WHITE_5PATTERNS = [
+    ['white', 'white', 'white', 'white', 'white'],
+    ['white', 'white', 'white', 'white', 'empty'],
+    ['empty', 'white', 'white', 'white', 'white'],
+    ['white', 'white', 'empty', 'white', 'white'],
+    ['white', 'empty', 'white', 'white', 'white'],
+    ['white', 'white', 'white', 'empty', 'white'],
+    ['white', 'empty', 'white', 'white', 'empty'],
+    ['empty', 'white', 'empty', 'white', 'white']
+]
 
-WHITE_5PATTERNS = [['white', 'white', 'white', 'white', 'white'],
-                   ['white', 'white', 'white', 'white', 'empty'],
-                   ['empty', 'white', 'white', 'white', 'white'],
-                   ['white', 'white', 'empty', 'white', 'white'],
-                   ['white', 'empty', 'white', 'white', 'white'],
-                   ['white', 'white', 'white', 'empty', 'white']]
-WHITE_5SCORES = [1000000,5000,5000,5000,5000,5000]
-# WHITE_5SCORES = [50000,720,720,720,720,720] #based on Dong (2015)
+WHITE_5SCORES = [1000000, 5000, 5000, 5000, 5000, 5000, 2000, 1500]
 
-BLACK_6PATTERNS = [['empty', 'black', 'black', 'black', 'black','empty'],
-                   ['empty', 'black', 'black', 'black', 'empty','empty'],
-                   ['empty', 'empty', 'black', 'black', 'black','empty'],
-                   ['empty', 'black', 'black', 'empty', 'black','empty'],
-                   ['empty', 'black', 'empty', 'black', 'black','empty'],
-                   ['empty', 'empty', 'black', 'black', 'empty','empty'],
-                   ['empty', 'empty', 'black', 'empty', 'black','empty'],
-                   ['empty', 'black', 'empty', 'black', 'empty','empty'],
-                   ['empty', 'empty', 'black', 'empty', 'empty','empty'],
-                   ['empty', 'empty', 'empty', 'black', 'empty','empty']]
-BLACK_6SCORES = [50000,5000,5000,500,500,100,100,100,10,10]
-# BLACK_6SCORES = [8640,720,720,720,720,120,120,120,20,20] #based on Dong (2015)
+WHITE_4PATTERNS = [
+    ['white', 'white', 'white', 'white'],
+    ['empty', 'white', 'white', 'white'],
+    ['white', 'white', 'empty', 'white'],
+    ['white', 'empty', 'white', 'white'],
+    ['empty', 'white', 'empty', 'white']
+]
 
+WHITE_4SCORES = [20000, 5000, 3000, 2000, 1500]
 
-BLACK_5PATTERNS = [['black', 'black', 'black', 'black', 'black'],
-                   ['black', 'black', 'black', 'black', 'empty'],
-                   ['empty', 'black', 'black', 'black', 'black'],
-                   ['black', 'black', 'empty', 'black', 'black'],
-                   ['black', 'empty', 'black', 'black', 'black'],
-                   ['black', 'black', 'black', 'empty', 'black']]
-BLACK_5SCORES = [1000000,5000,5000,5000,5000,5000]
-# BLACK_5SCORES = [50000,720,720,720,720,720] #based on Dong (2015)
+BLACK_6PATTERNS = [
+    ['empty', 'black', 'black', 'black', 'black', 'empty'],
+    ['empty', 'black', 'black', 'black', 'empty', 'empty'],
+    ['empty', 'empty', 'black', 'black', 'black', 'empty'],
+    ['empty', 'black', 'black', 'empty', 'black', 'empty'],
+    ['empty', 'black', 'empty', 'black', 'black', 'empty'],
+    ['empty', 'empty', 'black', 'black', 'empty', 'empty'],
+    ['empty', 'empty', 'black', 'empty', 'black', 'empty'],
+    ['empty', 'black', 'empty', 'black', 'empty', 'empty'],
+    ['empty', 'empty', 'black', 'empty', 'empty', 'empty'],
+    ['empty', 'empty', 'empty', 'black', 'empty', 'empty'],
+    ['black', 'black', 'empty', 'black', 'black', 'empty'],
+    ['empty', 'black', 'black', 'black', 'empty', 'black'],
+    ['black', 'empty', 'black', 'empty', 'black', 'empty']
+]
+
+BLACK_6SCORES = [50000, 5000, 5000, 500, 500, 100, 100, 100, 10, 10, 2500, 1500, 1000]
+
+BLACK_5PATTERNS = [
+    ['black', 'black', 'black', 'black', 'black'],
+    ['black', 'black', 'black', 'black', 'empty'],
+    ['empty', 'black', 'black', 'black', 'black'],
+    ['black', 'black', 'empty', 'black', 'black'],
+    ['black', 'empty', 'black', 'black', 'black'],
+    ['black', 'black', 'black', 'empty', 'black'],
+    ['black', 'empty', 'black', 'black', 'empty'],
+    ['empty', 'black', 'empty', 'black', 'black']
+]
+
+BLACK_5SCORES = [1000000, 5000, 5000, 5000, 5000, 5000, 2000, 1500]
+
+BLACK_4PATTERNS = [
+    ['black', 'black', 'black', 'black'],
+    ['empty', 'black', 'black', 'black'],
+    ['black', 'black', 'empty', 'black'],
+    ['black', 'empty', 'black', 'black'],
+    ['empty', 'black', 'empty', 'black']
+]
+
+BLACK_4SCORES = [20000, 5000, 3000, 2000, 1500]
 
 def sublist(small, big):
     '''
     Return True if small is a sublist of big.
     '''
-    for i in xrange(len(big)-len(small)+1):
-        for j in xrange(len(small)):
-            if big[i+j] != small[j]:
+    for i in range(len(big) - len(small) + 1):
+        for j in range(len(small)):
+            if big[i + j] != small[j]:
                 break
         else:
             return True
@@ -74,7 +109,6 @@ def enum_to_string(vector):
     
     return string_list
 
-
 def evaluate_vector(vector):
     '''
     Return the score for a vector (line or column or diagonal)
@@ -83,6 +117,7 @@ def evaluate_vector(vector):
     score = {'white': 0, 'black': 0}
     length = len(string_list)
 
+    # Check for five-piece patterns
     if length == 5:
         for i in range(len(WHITE_5PATTERNS)):
             if WHITE_5PATTERNS[i] == string_list:
@@ -91,16 +126,17 @@ def evaluate_vector(vector):
                 score['black'] += BLACK_5SCORES[i]
         return score
 
-    for i in range(length - 5):
-        temp = [string_list[i], string_list[i + 1], string_list[i + 2],
-                string_list[i + 3], string_list[i + 4]]
-        for i in range(len(WHITE_5PATTERNS)):
-            if WHITE_5PATTERNS[i] == temp:
-                score['white'] += WHITE_5SCORES[i]
-            if BLACK_5PATTERNS[i] == temp:
-                score['black'] += BLACK_5SCORES[i]
+    # Check for four-piece patterns
+    for i in range(length - 3):
+        temp = [string_list[i], string_list[i + 1], string_list[i + 2], string_list[i + 3]]
+        for j in range(len(WHITE_4PATTERNS)):
+            if WHITE_4PATTERNS[j] == temp:
+                score['white'] += WHITE_4SCORES[j]
+            if BLACK_4PATTERNS[j] == temp:
+                score['black'] += BLACK_4SCORES[j]
 
-    for i in range(length - 6):
+    # Check for six-piece patterns
+    for i in range(length - 5):
         temp = [
             string_list[i],
             string_list[i + 1],
@@ -108,16 +144,11 @@ def evaluate_vector(vector):
             string_list[i + 3],
             string_list[i + 4],
             string_list[i + 5],
-            ]
-        for i in range(len(WHITE_6PATTERNS)):
-            if WHITE_6PATTERNS[i] == temp:
-                score['white'] += WHITE_6SCORES[i]
-            if BLACK_6PATTERNS[i] == temp:
-                score['black'] += BLACK_6SCORES[i]
+        ]
+        for j in range(len(WHITE_6PATTERNS)):
+            if WHITE_6PATTERNS[j] == temp:
+                score['white'] += WHITE_6SCORES[j]
+            if BLACK_6PATTERNS[j] == temp:
+                score['black'] += BLACK_6SCORES[j]
+
     return score
-
-
-
-      
-
-        
