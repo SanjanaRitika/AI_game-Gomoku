8b91ec4a62dce6525d1cf1bffa05f004c489b220
SanjanaRitika <sanjanakarim24@gmail.com>
2024-11-13 02:25:10 +0600

    edited

--- board.js
+++ board.js
@@ -43,16 +43,21 @@ class Board {
         }
     }
 
+    // return array of rows of Squares
     getMatrix() {
         return this.matrix;
     }
 
+    // return array of rows of raw values
     getRawMatrix() {
         return this.matrix.reduce((arr, row) => {
+
+            // for each row, reduce to raw values
             row = row.reduce((a, c) => {
-                a.push(c.getVal());
+                a.push(c.getVal())
                 return a;
             }, []);
+
             arr.push(row);
             return arr;
         }, []);
@@ -62,6 +67,8 @@ class Board {
         return this.matrix[num];
     }
 
+    // remove empty squares that only necessary squares are evaluated
+    // optional padding will be applied around played squares
     static pruneMatrix(matrix, padding) {
         let left = Infinity, right = 0, top = Infinity, bottom = 0;
         const MATRIX_SIZE = 10;
@@ -79,11 +86,13 @@ class Board {
 
         if (padding) {
             left = Math.max(0, left - padding);
-            right = Math.min(MATRIX_SIZE, right + padding);
+            right = Math.min(MATRIX_SIZE - 1, right + padding);
             top = Math.max(0, top - padding);
-            bottom = Math.min(MATRIX_SIZE, bottom + padding);
+            bottom = Math.min(MATRIX_SIZE - 1, bottom + padding);
         }
 
+        // always return square matrix
+        // left: 0, right: 10, top: 0, bottom: 8
         if ((right - left) - (bottom - top)) {
             console.log('normalizing pruned size');
             console.log('left: %s, right: %s, top: %s, bottom: %s', left, right, top, bottom);
@@ -93,6 +102,7 @@ class Board {
             let size = Math.max(width, height);
 
             if (width !== size) {
+                // add half to left and right
                 let x = size - width;
                 left = Math.max(0, left - Math.floor(x / 2));
                 right = Math.min(9, right + Math.floor(x / 2));
@@ -120,8 +130,9 @@ class Board {
                 }
             }
 
+            // check results
             if ((bottom - top) !== (right - left)) {
-                console.log('error!! unequal sizes!!!');
+                console.log('error!! unequal sizes!!!')
             }
         }
 
@@ -129,9 +140,11 @@ class Board {
 
         for (let i = top; i <= bottom; i++) {
             let row = [];
+
             for (let j = left; j <= right; j++) {
                 row.push(matrix[i][j]);
             }
+
             copy.push(row);
         }
 
@@ -145,6 +158,7 @@ class Board {
     }
 
     static checkWinner3(matrix, hMask1, hMask2, vMask, dMask1, dMask2) {
+        // flatten matrix
         matrix = [].concat.apply([], [...matrix]);
 
         for (let i = 0; i < matrix.length; i++) {
@@ -161,17 +175,23 @@ class Board {
             if (matrix.length < mask.length + start) {
                 return false;
             }
+
             for (let i = 0; i < mask.length; i++) {
                 if (mask[i] && !matrix[start + i]) return false;
             }
+
             return true;
         }
     }
 
+
+    // enhance with bitmasks
     static checkWinner2(matrix) {
         const IN_A_ROW = 5;
-        for (let i = 0; i < matrix.length - IN_A_ROW; i++) {
-            for (let j = 0; j < matrix[i].length; j++) {
+
+        // move masks across matrix and check for matches
+        for (let i = 0; i < matrix.length - IN_A_ROW; i++) {    // i = 0 -> 5
+            for (let j = 0; j < matrix[i].length; j++) {      // j = 0 -> 9
                 let resH = 1, resV = 1, resD1 = 1, resD2 = 1;
 
                 for (let k = 0; k < IN_A_ROW; k++) {
@@ -184,24 +204,32 @@ class Board {
                 if (resH || resV || resD1 || resD2) return 1;
             }
         }
+
         return 0;
     }
 
+
+
+    // return 1 if human wins, -1 if AI wins, 0 for no winner
     static checkWinner(matrix) {
         for (let i = 0; i < matrix.length; i++) {
             let res = { hor: {}, ver: {}, dg1: {}, dg2: {}, dg3: {}, dg4: {} };
 
+            // build results obj
             for (let key in res) {
                 res[key] = { streak: 0, current: 0 };
             }
 
             for (let j = 0; j < matrix[i].length; j++) {
+                // check horizontals
                 let winner = check(matrix[i][j], res.hor);
                 if (winner) return winner;
 
+                // check verticals
                 winner = check(matrix[j][i], res.ver);
                 if (winner) return winner;
 
+                // check all four diagonals
                 if (i < 4 || j > i) continue;
 
                 let len = matrix.length;
@@ -216,12 +244,13 @@ class Board {
 
                 winner = check(matrix[len - 1 - i + j][len - 1 - j], res.dg4);
                 if (winner) return winner;
-            }
-        }
+            }// end inner for loop
+        }// end outer for loop
+
         return 0;
 
         function check(square, obj) {
-            if (square) {
+            if (!!square) {
                 if (square === obj.current) {
                     obj.streak++;
                 } else {
@@ -232,35 +261,58 @@ class Board {
                 obj.streak = 0;
                 obj.current = 0;
             }
-            return obj.streak === 5 ? obj.current : 0;
+
+            if (obj.streak === 5) {
+                // a player has won
+                return obj.current;
+            } else {
+                // no player has won
+                return 0;
+            }
+        }
+
+        function highlight(a, b) {
+            board.getSquare(a, b).twinkle();
         }
     }
 
     showAnimation() {
         return new Promise(async (resolve, reject) => {
+            // twinkle board
             let boardSize = board.getSize();
-            let squareList = Array.from({ length: boardSize * boardSize }, (k, v) => v);
+
+            // create list of numbers from 0 -> square of board size
+            let squareList = Array.from({
+                length: boardSize * boardSize
+            }, (k, v) => v);
+
+            // shuffle list
             squareList.sort(() => Math.random() - 0.5);
 
-            for (let i = 0; i < squareList.length; i += 10) {
-                let promises = Array.from({ length: 10 }, (k, v) => v);
+            for (let i = 0; i < squareList.length; i += 5) {
+                let promises = Array.from({ length: 5 }, (k, v) => v);
                 promises = promises.map(a => {
                     let num = squareList[i + a];
                     let x = num % boardSize, y = Math.floor(num / boardSize);
                     return board.getSquare(y, x).twinkle();
                 });
+
+                // twinkle square
                 await Promise.all(promises);
             }
 
+            // shuffle list again
             squareList.sort(() => Math.random() - 0.5);
 
-            for (let i = squareList.length - 1; i > 0; i -= 10) {
-                let promises = Array.from({ length: 10 }, (k, v) => v);
+            for (let i = squareList.length - 1; i > 0; i -= 5) {
+                let promises = Array.from({ length: 5 }, (k, v) => v);
                 promises = promises.map(a => {
                     let num = squareList[i - a];
                     let x = num % boardSize, y = Math.floor(num / boardSize);
                     return board.getSquare(y, x).untwinkle();
                 });
+
+                // untwinkle squares
                 await Promise.all(promises);
             }
 
